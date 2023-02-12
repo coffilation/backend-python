8177c576654bbbb651b9d1cc663b7697adc208b3
xalvaine <xalvaine@gmail.com>
2023-02-12 16:00:40 +0300

    [fix] Fix issues

--- apps/places/models.py
+++ apps/places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.constraints import UniqueConstraint
 
 
 class Place(models.Model):
@@ -7,10 +8,10 @@ class Place(models.Model):
     longitude = models.FloatField()
     osm_id = models.BigIntegerField()
     osm_type = models.CharField(max_length=255)
-    display_name = models.CharField(max_length=1023)
+    display_name = models.TextField(max_length=2000)
     category = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
     address = models.JSONField()
 
     class Meta:
-        unique_together = ['osm_id', 'osm_type', 'category']
+        constraints = [UniqueConstraint(fields=['osm_id', 'osm_type', 'category'], name='place_osm_identificators')]
